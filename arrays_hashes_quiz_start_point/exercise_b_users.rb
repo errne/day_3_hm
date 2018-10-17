ab151987b808781371db752534e22d66066c69ea
Ernest Fraiman <upapaus@gmail.com>
2018-10-17 20:56:48 +0100

    Finished all tasks in exercise B, also added some honesty

--- arrays_hashes_quiz_start_point/exercise_b_users.rb
+++ arrays_hashes_quiz_start_point/exercise_b_users.rb
@@ -4,42 +4,42 @@ users = {
     :lottery_numbers => [6, 12, 49, 33, 45, 20],
     :home_town => "Stirling",
     :pets => [
-    {
-      :name => "fluffy",
-      :species => "cat"
-    },
-    {
-      :name => "fido",
-      :species => "dog"
-    },
-    {
-      :name => "spike",
-      :species => "dog"
-    }
-  ]
+      {
+        :name => "fluffy",
+        :species => "cat"
+      },
+      {
+        :name => "fido",
+        :species => "dog"
+      },
+      {
+        :name => "spike",
+        :species => "dog"
+      }
+    ]
   },
   "Erik" => {
     :twitter => "eriksf",
     :lottery_numbers => [18, 34, 8, 11, 24],
     :home_town => "Linlithgow",
     :pets => [
-    {
-      :name => "nemo",
-      :species => "fish"
-    },
-    {
-      :name => "kevin",
-      :species => "fish"
-    },
-    {
-      :name => "spike",
-      :species => "dog"
-    },
-    {
-      :name => "rupert",
-      :species => "parrot"
-    }
-  ]
+      {
+        :name => "nemo",
+        :species => "fish"
+      },
+      {
+        :name => "kevin",
+        :species => "fish"
+      },
+      {
+        :name => "spike",
+        :species => "dog"
+      },
+      {
+        :name => "rupert",
+        :species => "parrot"
+      }
+    ]
   },
   "Avril" => {
     :twitter => "bridgpally",
@@ -67,7 +67,22 @@ lottery_numbers = users["Erik"][:lottery_numbers]
 p lottery_numbers.sort[0]
 p lottery_numbers.min()
 # 6. Return an array of Avril's lottery numbers that are even
-p users["Avril"][:lottery_numbers].select(&:even?)
+
+users["Avril"][:lottery_numbers].select(&:even?) #we found this on stackoverflow
+#tried to fully understand, gave up. Below what I come up by myself:
+
+lottery_array = users["Avril"][:lottery_numbers]
+def return_even_numbers(array)
+  even_numbers = []
+  for number in array
+    if ( number%2 == 0 )
+      even_numbers.push(number)
+    end
+  end
+  return even_numbers
+end
+p return_even_numbers(lottery_array)
+
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 users["Erik"][:lottery_numbers].push(7)
 p users["Erik"][:lottery_numbers].to_s
@@ -77,9 +92,7 @@ p "Erik's hometown is " + users["Erik"][:home_town].to_s
 # 9. Add a pet dog to Erik called "Fluffy"
 p users["Erik"][:pets].push({:name => "Fluffy", :species => "dog"})
 # 10. Add another person to the users hash
-pets_array = [{:name => "Fiffy", :species => "pig"}]
-p pets_array
-"Steven" = {:twitter => "Ste607967",
-    :lottery_numbers => [8, 33, 28, 15, 27],
-    :home_town => "Glasgow"} #:pets => [{:name => "Fiffy", :species => "pig"}]}
-    p "Steven"
+
+users["Steven"] = {:twitter => "Ste607967", :lottery_numbers => [8, 33, 28, 15, 27],
+  :home_town => "Glasgow", :pets =>[{:name => "Fiffy", :species => "crocodile"}]}
+  p users
